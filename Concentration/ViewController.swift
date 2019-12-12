2516db18f419fb9294344a62991089bad99a5dcc
Flax <present_xm@qq.com>
2019-12-12 15:10:47 +0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by 須木 on 2019/12/12.
+//  Copyright © 2019 須木. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
